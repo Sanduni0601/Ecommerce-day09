2800bcb68e90003dbaf53aa49723f658de6522b9
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-17 18:18:53 +0530

    Create Employee.java

---
+++ day09/src/main/java/fas/ict/day09/model/Employee.java
@@ -0,0 +1,24 @@
+package fas.ict.day09.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee extends Person{
+	@Id
+	private String empNo;
+	private double salary;
+	
+	@ManyToOne
+	private Department department; // one - many relationship in employee and department
+	
+	@ManyToMany(mappedBy="employees")
+	private List<Project>projects; //employee has many projects
+}
+
